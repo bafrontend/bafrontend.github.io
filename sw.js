62d420b25b7166e1ecb9b466e15bdcc93af07736
Guille Paz <guille87paz@gmail.com>
2015-08-08 15:50:50 -0300

    Build v0.0.1

---
+++ sw.js
@@ -0,0 +1,75 @@
+'use strict';
+
+importScripts('/serviceworker-cache-polyfill.js');
+
+var CACHE_NAME = 'bafrontend';
+var CACHE_VERSION = '0.0.1';
+
+var urlsToCache = [
+  '/',
+  '/src/css/styles.min.css',
+  '/assets/baf-logo.png',
+  '/assets/city.png',
+  '/assets/frontenders.png',
+  '/assets/sponsors/auth0.jpeg',
+  '/assets/sponsors/aerolab.jpeg',
+  '/assets/sponsors/davinci.jpeg',
+  '/assets/sponsors/flowics.jpeg',
+  '/assets/sponsors/mango.jpeg',
+  '/assets/sponsors/mercadolibre.jpeg',
+  '/assets/sponsors/px2html.jpeg',
+  '/assets/icons/twitter.svg',
+  '/assets/icons/github.svg',
+  '/assets/icons/linkedin.svg',
+  '/assets/Sun/Sun-ExtraLight.ttf',
+  '/favicon.ico'
+];
+
+/**
+ * Install
+ */
+this.addEventListener('install', function(eve) {
+  var urls = urlsToCache.map(function(url) {
+    return new Request(url, {credentials: 'include'});
+  });
+  '/src/assets/frontenders.png',
+  eve.waitUntil(
+    caches.open(CACHE_NAME + '-v' + CACHE_VERSION)
+      .then(function(cache) {
+        return cache.addAll(urls);
+      })
+  );
+});
+
+/**
+ * Active / Remove old caches
+ */
+this.addEventListener('active', function(eve) {
+  var currentCacheName = CACHE_NAME + '-v' + CACHE_VERSION;
+  caches.keys().then(function(cacheNames) {
+    return Promise.all(
+      cacheNames.map(function(cacheName) {
+        if (cacheName.indexOf(CACHE_NAME) == -1) {
+          return;
+        }
+
+        if (cacheName != currentCacheName) {
+          return caches.delete(cacheName);
+        }
+      })
+    );
+  });
+});
+
+/**
+ * Fetch
+ */
+this.addEventListener('fetch', function(eve) {
+  var request = eve.request;
+  var url = new URL(request.url);
+  eve.respondWith(
+    caches.match(request).then(function(response) {
+      return response || fetch(request);
+    })
+  );
+});
