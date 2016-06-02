ac68ac27a223ff5b11ace96529b933fed0c292ff
Guille Paz <guillermo@getmango.com>
2016-06-02 15:53:31 -0300

    Add new meetup.

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 importScripts('/serviceworker-cache-polyfill.js');
 
 var CACHE_NAME = 'bafrontend';
-var CACHE_VERSION = '0.0.21';
+var CACHE_VERSION = '0.0.22';
 
 var urlsToCache = [
   '/',
@@ -16,6 +16,7 @@ var urlsToCache = [
   '/assets/sponsors/auth0.jpeg',
   '/assets/sponsors/aerolab.jpeg',
   '/assets/sponsors/digitalhouse.png',
+  '/assets/sponsors/davinci.jpeg',
   '/assets/sponsors/flowics.jpeg',
   '/assets/sponsors/mercadolibre.jpeg',
   '/assets/icons/twitter.svg',
