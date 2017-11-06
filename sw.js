fd7015a1dc240a01e17affd44a584500c3ba5574
Roberto Gonzalez <roberto@aerolab.co>
2017-11-06 17:43:14 -0300

    Updated share img

--- sw.js
+++ sw.js
@@ -3,15 +3,20 @@
 Cache.prototype.add||(Cache.prototype.add=function(t){return this.addAll([t])}),Cache.prototype.addAll||(Cache.prototype.addAll=function(t){function e(t){this.name="NetworkError",this.code=19,this.message=t}var r=this;return e.prototype=Object.create(Error.prototype),Promise.resolve().then(function(){if(arguments.length<1)throw new TypeError;return t=t.map(function(t){return t instanceof Request?t:t+""}),Promise.all(t.map(function(t){"string"==typeof t&&(t=new Request(t));var r=new URL(t.url).protocol;if("http:"!==r&&"https:"!==r)throw new e("Invalid scheme");return fetch(t.clone())}))}).then(function(e){return Promise.all(e.map(function(e,n){return r.put(t[n],e)}))}).then(function(){return void 0})});
 
 var CACHE_NAME = 'bafrontend';
-var CACHE_VERSION = '2.1.0';
+var CACHE_VERSION = '2.2.0';
 
 var urlsToCache = [
   '/',
   '/scripts/main.js',
   '/styles/main.css',
 
-  '/images/speakers/celina.jpg',
-  '/images/speakers/dan.jpg',
+  '/images/speakers/celina-twardowski.jpg',
+  '/images/speakers/dan-zajdband.jpg',
+  '/images/speakers/eva-ferreira.jpg',
+  '/images/speakers/leo-pittelli.jpg',
+
+  '/images/utn/utn-1.jpg',
+  '/images/utn/utn-2.jpg',
 
   '/images/aerolab.svg',
   '/images/arrow-black.svg',
